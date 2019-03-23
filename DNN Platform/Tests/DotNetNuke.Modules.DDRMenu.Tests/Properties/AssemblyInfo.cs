8f3d7fa532b1563345b34babb4971d7a7d883ece
Andrew Hoefling <andrew@hoeflingsoftware.com>
2019-03-22 23:05:37 -0400

    Scaffolded unit test project for DDRMenu

---
+++ DNN Platform/Tests/DotNetNuke.Modules.DDRMenu.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetNuke.Modules.DDRMenu.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetNuke.Modules.DDRMenu.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0103d303-da51-4671-946e-a48d83bff0fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

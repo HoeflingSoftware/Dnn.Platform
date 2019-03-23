19ff560f60414a5abb39f416f594f44b678dc8c9
Andrew Hoefling <andrew@hoeflingsoftware.com>
2019-03-22 23:16:08 -0400

    Added null tests for the Razor Template render logic

--- DNN Platform/Tests/DotNetNuke.Modules.DDRMenu.Tests/TemplateEngine/RazorTemplateProcessorTests.cs
+++ DNN Platform/Tests/DotNetNuke.Modules.DDRMenu.Tests/TemplateEngine/RazorTemplateProcessorTests.cs
@@ -1,5 +1,8 @@
 ﻿using DotNetNuke.Web.DDRMenu.TemplateEngine;
 using NUnit.Framework;
+using System.IO;
+using System.Text;
+using System.Web.UI;
 
 namespace DotNetNuke.Modules.DDRMenu.TemplateEngine.Tests
 {
@@ -51,6 +54,48 @@ namespace DotNetNuke.Modules.DDRMenu.TemplateEngine.Tests
             bool actual = _processor.LoadDefinition(definition);
 
             Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void Render_Invalid_Null_VirtualPath_Test()
+        {
+            StringBuilder builder = new StringBuilder();
+            StringWriter writer = new StringWriter(builder);
+            HtmlTextWriter htmlWriter = new HtmlTextWriter(writer);
+
+            TemplateDefinition definition = new TemplateDefinition();
+            definition.TemplateVirtualPath = null;
+
+            _processor.Render(null, htmlWriter, definition);
+
+            Assert.IsEmpty(builder.ToString());
+        }
+
+        [Test]
+        public void Render_Invalid_Empty_VirtualPath_Test()
+        {
+            StringBuilder builder = new StringBuilder();
+            StringWriter writer = new StringWriter(builder);
+            HtmlTextWriter htmlWriter = new HtmlTextWriter(writer);
+
+            TemplateDefinition definition = new TemplateDefinition();
+            definition.TemplateVirtualPath = string.Empty;
+
+            _processor.Render(null, htmlWriter, definition);
+
+            Assert.IsEmpty(builder.ToString());
+        }
+
+        [Test]
+        public void Render_Invalid_Null_TemplateDefinition_Test()
+        {
+            StringBuilder builder = new StringBuilder();
+            StringWriter writer = new StringWriter(builder);
+            HtmlTextWriter htmlWriter = new HtmlTextWriter(writer);
+
+            _processor.Render(null, htmlWriter, null);
+
+            Assert.IsEmpty(builder.ToString());
         }
     }
 }
